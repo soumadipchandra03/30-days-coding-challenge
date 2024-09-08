4199393e701f5e98d84a7167bce91f5d718c7ef6
soumadipchandra <142772709+soumadipchandra03@users.noreply.github.com>
2024-09-09 00:46:18 +0530

    Add files via upload

---
+++ day1(if statement).java
@@ -0,0 +1,17 @@
+public class main{
+    public static void main(String[]args){
+        int age=80;
+        if(age>=75){
+            System.out.println("you are a seniour citizen:");
+        }
+        else if(age>=18) {
+            System.out.println("you are an audult:");
+        }
+        else{
+            System.out.println("you are not audult:");
+            
+        }
+        
+    }
+    
+}
