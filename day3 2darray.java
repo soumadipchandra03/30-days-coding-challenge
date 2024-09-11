87ffe5a28a1c1f6685bf523fb3d0c72215f85d4d
soumadipchandra <142772709+soumadipchandra03@users.noreply.github.com>
2024-09-12 01:27:55 +0530

    Add files via upload

---
+++ day3 2darray.java
@@ -0,0 +1,21 @@
+public class main{
+    public static void main(String[]args){
+        String[][] cars={
+                          {"TATA","MAHINDRA","MARUTI","MG"},
+                          {"BMW","MUSTANG","SKODA","ALTO"}
+                        };
+        for (int i=0;i<cars.length;i++){
+            System.out.println();
+            for(int j=0;j<cars[i].length;j++){
+                System.out.println(cars[i][j]+"");
+                
+            }
+            
+            
+
+            
+        }
+        
+    
+    }
+}
